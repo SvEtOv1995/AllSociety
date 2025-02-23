45ee55d97695178d9a21298ab781dd0e1a39a648
mikhail <mishasvetov33@gmail.com>
2025-02-23 16:38:08 +0300

    pereodicAdded)

--- education/calculator/urls.py
+++ education/calculator/urls.py
@@ -3,6 +3,7 @@ from .views import algebra_view, geometry_view
 from . import views
 from .views import get_entries
 from .views import subject_calculator_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.index, name='index'),  # Главная страница (запускается сразу)
@@ -10,5 +11,6 @@ urlpatterns = [
     path('geometry/', views.geometry_view, name='geometry'),
     path('fizika/', views.fizika_view, name='fizika'),
     path('get_entries/', get_entries, name='get_entries'),
+    path('periodic-table/', TemplateView.as_view(template_name='periodic-table.html'), name='periodic_table'),
     path('<int:subject_id>/', subject_calculator_view, name='subject_calculator'),
 ]
